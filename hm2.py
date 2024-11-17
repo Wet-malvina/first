8d526e8b54f1b47423d46de4a51f42708793fe11
Wet-malvina <odealopuhovoe@gmail.com>
2024-11-18 01:42:12 +0300

    Create hm2.py

---
+++ hm2.py
@@ -0,0 +1,8 @@
+class Store
+    def __init__(self, name, adress, items):
+        self.name = name
+        self.adress = adress
+        self.items = items={}
+
+    def add_prod (self):
+        items.update
